bfbd133d2075dfe5b3e7c08e373de4eaa06a1f17
Feng Yin <feng.yin@live.cn>
2025-12-15 10:37:11 +0800

    d11

--- Day11/solution.py
+++ Day11/solution.py
@@ -1,5 +1,7 @@
 import re
 import copy
+import functools
+import sys
 
 def q1(maze, start, end):
     path_stack = []
@@ -31,134 +33,38 @@ def q1(maze, start, end):
                     path_stack.append(tmp_path_cp)
     return result
 
-def debug(maze):
-    ks = []
-    for k, v in maze.items():
-        if "out" in v:
-            #print("remove: ", (k, v))
-            ks.append(k)
-    for k in ks:
-        remove_invalid(maze, k, "out")
-    
-    print(maze)
 
-def remove_invalid(maze, start, end):
+def count_path(maze, start, end):
     if start not in maze:
-        return
-    print("remove ", end, " from ", start, maze[start])
-    maze[start] = list(set(maze[start]).symmetric_difference(set([end])))
-    #print("after removal: ", maze[start])
-    print("*"*30)
-    if len(maze[start]) == 0:
-        del maze[start]
-        next_starts = []
-        for k, v in maze.items():
-            if start in v:
-                next_starts.append(k)
-        for ns in next_starts:
-            remove_invalid(maze, ns, start)
+        return 0
 
-def q2_debug(maze, start, end):
-    path_stack = []
-    path_db = []
-    for p in maze[start]:
-        if p == end:
-            result += 1
-        else:
-            path_stack.append([p])
-    loops = 0
-    while len(path_stack) > 0:
-        loops += 1
-        if loops % 100000 == 0:
-            print("stack depth: ", len(path_stack))
-            print("path db: ", path_db)
-        tmp_path = path_stack.pop()
-        if tmp_path[-1] not in maze:
-            if len(tmp_path) > 1:
-                for i in reversed(range(len(path_stack))):
-                    if tmp_path in path_stack[i]:
-                        path_stack.pop(i)
-            #print("remove ", tmp_path[-1], " from ", maze[tmp_path[-2]])
-            #maze[tmp_path[-2]] = list(set(maze[tmp_path[-2]]).symmetric_difference(set([tmp_path[-1]])))
-            #print("after removal: ", maze[tmp_path[-2]])
-            continue
-        next_stops = maze[tmp_path[-1]]
-        for p in next_stops:
-            if p == end:
-                path_str = ",".join(tmp_path) + "," + end
-                if path_str not in path_db:
-                    path_db.append(path_str)
-                else:
-                    print("foud dup path")
-            else:
-                if p not in tmp_path:
-                    tmp_path_cp = copy.deepcopy(tmp_path)
-                    tmp_path_cp.append(p)
-                    path_stack.append(tmp_path_cp)
-                else:
-                    print("found a loop :", ",".join(tmp_path) + "," + p)
-        #loops -= 1
-        #if loops == 0:
-        #    break
-    #print(path_db)
-    print("*"*20)
-    print(maze)    
-    
-    return len(path_db)
+    @functools.lru_cache(maxsize=None)
+    def _count_path(paths, end):
+        if end in paths:
+            return 1
+        count = 0
+        for stop in paths:
+            if stop not in maze:
+                return 0
+            count += _count_path(tuple(maze[stop]), end)
+        return count
+    start_to_dac = _count_path(tuple(maze[start]), "dac")
+    start_to_fft = _count_path(tuple(maze[start]), "fft")
+    dac_to_fft = _count_path(tuple(maze["dac"]), "fft")
+    fft_to_dac = _count_path(tuple(maze["fft"]), "dac")
+    dac_to_end = _count_path(tuple(maze["dac"]), end)
+    fft_to_end = _count_path(tuple(maze["fft"]), end)
 
-def cleanup_invalid_path(maze, tmp_path):
-    for i in reversed(range(len(tmp_path))):
-        if len(maze[tmp_path[i-1]]) == 1:
-            continue
-        else:
-            for k in range(len(maze[tmp_path[i-1]])):
-                if maze[tmp_path[i-1]][k] == tmp_path[i]:
-                    print("remove ", tmp_path[i], " from ", tmp_path[i-1], maze[tmp_path[i-1]])
-                    maze[tmp_path[i-1]].pop(k)
-                    return
-    
+    p1 = start_to_dac*dac_to_fft*fft_to_end
+    p2 = start_to_fft*fft_to_dac*dac_to_end
+    return p1+p2
+
 def q2(maze, start, end):
-    path_stack = []
-    path_db = []
-    for p in maze[start]:
-        path_stack.append([start, p])
-    loops = 0
-    while len(path_stack) > 0:
-        loops += 1
-        if loops % 100000 == 0:
-            print("stack depth: ", len(path_stack))
-            print("stack path len: ", len(path_stack[-1]))
-            print("path db: ", len(path_db))
-        tmp_path = path_stack.pop()
-        next_stops = maze[tmp_path[-1]]
-        for p in next_stops:
-            if p == end:
-                path_str = ",".join(tmp_path) + "," + end
-                if path_str not in path_db:
-                    if "dac" in path_str and "fft" in path_str:
-                        path_db.append(path_str)
-                    #else:
-                        #print("invalid path: ", path_str)
-                        #cleanup_invalid_path(maze, tmp_path)
-                        #return 0
-                else:
-                    print("foud dup path: ", path_str)
-            else:
-                if p not in tmp_path:
-                    tmp_path_cp = copy.deepcopy(tmp_path)
-                    tmp_path_cp.append(p)
-                    path_stack.append(tmp_path_cp)
-                else:
-                    print("found circle", ",".join(tmp_path)+","+p)
-    #for p in path_db:
-    #    if "dac" in p and "fft" in p:
-    #        result += 1
-    #for ps in path_stack:
-    #    print(ps)
-    return len(path_db)
-
+    return count_path(maze, start, end)
+       
 with open("./input2.txt", "r") as file:
     maze = {}
+
     for line in file:
         line = line.strip()
         kv = line.split(':')
@@ -166,9 +72,6 @@ with open("./input2.txt", "r") as file:
         value = kv[1].strip().split()
         #print(key, value)
         maze[key] = value
-        
-    print("part 1: ", q1(maze, "you", "out"))
-    #print("part 1: ", q1(maze, "fft", "dac"))
-    #print("svr -> fft", q2_debug(maze, "svr", "fft"))
+
+    #print("part 1: ", q1(maze, "you", "out"))
     print("part 2: ", q2(maze, "svr", "out"))
-    #debug(maze)
